aa931ec97f4729821d4812050abe72fe0e4a3241
atlas-github <kohwyhow@gmail.com>
2022-03-30 00:49:26 +0800

    updated homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
+  <div
+    class="flex-auto h-screen bg-black h-[100vh] align-bottom text-white text-center z-10"
+  >
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+      <h1 class="text-5xl">Welcome to the Resistance!</h1>
+      <p class="mt-5">Stop the use of Exotic Matter.</p>
     </div>
+    <video
+      autoplay
+      loop
+      muted
+      class="absolute min-w-full max-h-fit max-w-fit align-center"
+    >
+      <source
+        src="https://ingress.com/assets/video/home-page-video.mp4"
+        type="video/mp4"
+      />
+    </video>
   </div>
 </template>
