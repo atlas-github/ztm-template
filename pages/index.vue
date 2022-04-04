a6e773f3032f4c3e2d9f5983bda6a753d9b1d1fc
atlas-github <kohwyhow@gmail.com>
2022-04-04 12:24:33 +0800

    changed

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,54 @@
 <template>
-  <div
-    class="flex-auto h-screen bg-black h-[100vh] align-bottom text-white text-center z-10"
-  >
-    <div class="m-auto">
-      <h1 class="text-5xl">Welcome to the Resistance!</h1>
-      <p class="mt-5">Stop the use of Exotic Matter.</p>
+  <div>
+    <div id="header" class="bg-[url('/hero-bg.jpg')] px-20">
+      <div id="menu" class="grid grid-cols-2 py-5">
+        <div id="menu-left">
+          <img src="logo.png" class="w-[150px]" alt="" />
+        </div>
+        <div id="menu-right">
+          <ul class="grid grid-cols-4 text-lg font-oswald text-white uppercase pt-5">
+            <li class="text-ellipsis overflow-hidden">About Us</li>
+            <li class="text-ellipsis overflow-hidden">Our Menu</li>
+            <li class="text-ellipsis overflow-hidden">Location</li>
+            <li class="text-ellipsis overflow-hidden">Contacts</li>
+          </ul>
+        </div>
+      </div>
+      <div class="grid grid-cols-2 pb-20 pt-10">
+        <img src="hero-burger.png" class="pr-8 mt-10" alt="" />
+        <div>
+          <h1 class="text-8xl text-white text-center font-lilita font-bold uppercase">
+            Classic Burger
+          </h1>
+          <h2
+            class="text-6xl text-white text-center font-lilita font-bold uppercase my-5"
+          >
+            With Onion
+          </h2>
+          <p class="text-white font-roboto font-light text-center">
+            Our signature burger now comes with onion and cheese. You can customize it to
+            your likings. Freshly made to order, all in a Brioche style bun.
+          </p>
+        </div>
+      </div>
     </div>
-    <video
-      autoplay
-      loop
-      muted
-      class="absolute min-w-full max-h-fit max-w-fit align-center"
+    <div
+      id="section-1"
+      class="text-center bg-[url('/three-burgers.jpeg')] pb-60 bg-cover bg-center"
     >
-      <source
-        src="https://ingress.com/assets/video/home-page-video.mp4"
-        type="video/mp4"
-      />
-    </video>
+      <h1 class="text-6xl uppercase font-oswald text-center pt-20 px-24">
+        The burger tastes better when you eat it with family
+      </h1>
+      <p class="font-oswald font-light text-gray-500 text-center text-lg py-6 px-28">
+        Spend quality time with your family and eat an affordable, nutritious and
+        delicious meal together. Not all burgers are fast foods. All our burgers are
+        handmade to orders for you an your family members to enjoy.
+      </p>
+      <div class="text-center">
+        <button class="font-oswald uppercase bg-red-500 text-white py-3 px-8">
+          Explore Full Menu
+        </button>
+      </div>
+    </div>
   </div>
 </template>
