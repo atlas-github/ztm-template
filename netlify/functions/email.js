9387a554fcfedd0e817789f8103526adb2775d63
atlas-github <kohwyhow@gmail.com>
2022-04-19 23:33:49 +0800

    hotmail.com

--- netlify/functions/email.js
+++ netlify/functions/email.js
@@ -16,7 +16,7 @@ exports.handler = async function (event, context) {
   emailContent = emailContent + `\n Total Amount: ${total.toFixed(2)}`;
 
   const email = {
-    from: 'kohwyhow@gmail.com',
+    from: 'kohwyhow@hotmail.com',
     to: customerEmail,
     subject: 'New Order Received',
     text: emailContent,
